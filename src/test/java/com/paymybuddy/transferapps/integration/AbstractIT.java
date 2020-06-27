5c7d81985bbf6f3c69bc68896200a3b9c4f82e3a
Antoine Engelaere <antoine.engelaere@cacd2.fr>
2020-06-27 13:08:52 +0200

    Fix

--- src/test/java/com/paymybuddy/transferapps/integration/AbstractIT.java
+++ src/test/java/com/paymybuddy/transferapps/integration/AbstractIT.java
@@ -5,12 +5,10 @@ import com.paymybuddy.transferapps.repositories.BankAccountRepository;
 import com.paymybuddy.transferapps.repositories.RelativeEmailRepository;
 import com.paymybuddy.transferapps.repositories.TransactionRepository;
 import com.paymybuddy.transferapps.repositories.UserAccountRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,7 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @WithMockUser(authorities = "ADMIN", username = "test@test.com")
 @AutoConfigureMockMvc(addFilters = false)
-@Sql(scripts={"classpath:test_data_init.sql"})
+@Sql(scripts = {"classpath:test_data_init.sql"})
 public abstract class AbstractIT {
 
     @Autowired
@@ -35,10 +33,4 @@ public abstract class AbstractIT {
     @Autowired
     protected TransactionRepository transactionRepository;
 
-    @AfterEach
-    void cleanup() {
-        applicationContext.getBeansOfType(CrudRepository.class)
-                .values()
-                .forEach(CrudRepository::deleteAll);
-    }
 }
